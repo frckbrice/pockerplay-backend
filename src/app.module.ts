0c19f5544b6afa0a2f640524c7cc2f94694e23e1
frckbrice <maebrie2017@gmail.com>
2023-12-20 22:02:23 +0100

    basic resource config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesModule } from './messages/messages.module';
+import { UsersModule } from './users/users.module';
+import { ChoiceModule } from './choice/choice.module';
+import { GameSessionModule } from './game_session/game_session.module';
+import { GameRoundModule } from './game_round/game_round.module';
+import { InvitationModule } from './invitation/invitation.module';
+import { OptionModule } from './option/option.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule, UsersModule, ChoiceModule, GameSessionModule, GameRoundModule, InvitationModule, OptionModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: console,
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
