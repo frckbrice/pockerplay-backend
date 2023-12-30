69b3180f20d42fe9056df290ac7fe784c44b84ec
AVOM  Brice <maebrie2017@gmail.com>
2023-12-30 19:13:39 +0100

    Update main.ts
    update port in main

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3002);
+  await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
   // console.log(dbconfig);
 }
